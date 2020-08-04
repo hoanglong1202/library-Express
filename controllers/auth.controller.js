dbf28a7a344d0cb29785636c74787b696bf63172
Accel <cyberpunk2076@gmail.com>
2020-08-04 22:38:00 +0700

    add bcrypt harsh

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 //lấy khai báo lowdb từ db.js
 var db = require('../db');
 var md5 = require('md5');
+var bcrypt = require('bcrypt');
 
 module.exports.index = function (req, res) {
     res.render('auth/index');
@@ -40,23 +41,55 @@ module.exports.postLogin = function (req, res) {
         return;
     }
 
-    var harshPass = md5(pass);
-
-    if (user.pass !== harshPass) {
-        errors = 'Password không đúng';
-        res.render('auth/index', {
-            errors: errors,
-            value: req.body
-        });
-        return;
+    var checkLegit = Number(user.wrongLoginCount);
+    console.log(checkLegit);
+    if (checkLegit > 4){
+        return res.render('404');
     }
 
-    res.cookie('userID', user.id);
-    if (user.isAdmin === "true") {
-        res.cookie('isAdmin', true);
-        res.redirect('/users');
-    } else {
-        res.cookie('isAdmin', false);
-        res.redirect('/trans');
-    }
+    // bcrypt.hash('123123', 10,function(err, hash) {
+    //     console.log(hash);
+    // });
+
+    bcrypt.compare(pass, user.pass, function (err, result) {
+        if (!result) {
+            //set số lần nhập sai pass
+            var countWrong = Number(user.wrongLoginCount);
+            countWrong++;
+            db.get('users').find({
+                email: email
+            }).assign({
+                wrongLoginCount: countWrong
+            }).write();
+
+            errors = 'Password không đúng';
+            res.render('auth/index', {
+                errors: errors,
+                value: req.body
+            });
+            return;
+        }
+
+        //nếu pass nhập đúng
+        //server gửi cookie cho client
+        res.cookie('userID', user.id);
+
+        //set lại số lận nhập sai pass
+        var countWrong = 0;
+        db.get('users').find({
+            email: email
+        }).assign({
+            wrongLoginCount: countWrong
+        }).write();
+
+        if (user.isAdmin === "true") {
+            res.cookie('isAdmin', true);
+            res.redirect('/users');
+        } else {
+            res.cookie('isAdmin', false);
+            res.redirect('/trans');
+        }
+    });
+
+
 }
